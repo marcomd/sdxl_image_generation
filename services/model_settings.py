640289c6688fed7b952757dd67e9c3cea32845d3
Marco Mastrodonato <m.mastrodonato@gmail.com>
2025-03-02 18:39:06 +0100

    Added sdxl-anime-legacy model in order to compare the previous animagine

--- services/model_settings.py
+++ services/model_settings.py
@@ -3,6 +3,12 @@ from typing import Dict, Any
 DEFAULT_NEGATIVE_PROMPT: str = "lowres, (bad), text, error, fewer, extra, missing, worst quality, jpeg artifacts, low quality, watermark, unfinished, displeasing, oldest, early, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract]"
 
 MODEL_SETTINGS: Dict[str, Dict[str, Any]] = {
+    "sdxl-anime-legacy": {
+        "model_id": "cagliostrolab/animagine-xl-3.1",
+        "num_inference_steps": 28,
+        "guidance_scale": 7.0,
+        "negative_prompt": DEFAULT_NEGATIVE_PROMPT
+    },
     "sdxl-anime": {
         "model_id": "cagliostrolab/animagine-xl-4.0",
         "num_inference_steps": 28,
